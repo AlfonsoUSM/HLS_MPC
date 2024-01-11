a1f933b7c42bf7809e95c0b706f9b07be1683c82
AlfonsoUSM <39561772+AlfonsoUSM@users.noreply.github.com>
2024-01-11 14:46:46 -0300

    Fixed Matlab version for DER
    - fixed constraints

--- matlab/fx_qp_admm.m
+++ matlab/fx_qp_admm.m
@@ -5,31 +5,31 @@
 % Based on the work by Juan David Escárate
 % ===============================================================================
 
-function [tk, zk, uk] = fx_qp_admm(Q, q, H, h, init_tk, init_zk, init_uk, rho, iters)          
+function [tk, zk, uk] = fx_qp_admm(Q, q, G, g, init_tk, init_zk, init_uk, rho, iters)          
 %     global QP_vars counter
-    % ------ QP PROBLEM ------ %
-    % Min: 0.5*tk'*H*tk + h'*tk
-    % ST:  M*tk <= c
-    % 
-    % ADMM Reformulation
-    % Min: 0.5*tk'*H*tk + h'*tk + g(zk)
-    % ST:  M*tk + zk = c
-    % g(zk) is the indicator function of Z:
-        % g(zk) = 0 if zk in Z,
-        % g(zk) = ∞ if any component of zk not in Z
-    %
-    % Q -> R^(n x n)
-    % A -> R^(n x n)
-    % q -> R^(n)
-    % c -> R^(n)
-    % x -> R^(n)
-    % z -> R^(n) (slack variable)
-    
-    % init_tk -> Initial tk point
-    % init_zk -> Initial zk point
-    % init_uk -> Initial uk point
-    % rho -> Rho value for Augmented Lagrangian
-    % iters -> Maximum iterations
+% ------ QP PROBLEM ------ %
+% Min: 0.5*tk'*Q*tk + q'*tk
+% ST:  G*tk <= g
+% 
+% ADMM Reformulation
+% Min: 0.5*tk'*Q*tk + q'*tk + h(zk)
+% ST:  G*tk + zk = g
+% h(zk) is the indicator function of Z:
+%   h(zk) = 0 if zk in Z,
+%   h(zk) = ∞ if any component of zk not in Z
+%
+% Q -> R^(n x n)
+% G -> R^(n x n)
+% q -> R^(n)
+% g -> R^(n)
+% x -> R^(n)
+% z -> R^(n) (slack variable)
+
+% init_tk -> Initial tk point
+% init_zk -> Initial zk point
+% init_uk -> Initial uk point
+% rho -> Rho value for Augmented Lagrangian
+% iters -> Maximum iterations
     
     % ------ CHECK NUMBER OF INPUT ARGUMENTS ------ %
     if nargin < 9 || nargin > 9
@@ -39,22 +39,11 @@ function [tk, zk, uk] = fx_qp_admm(Q, q, H, h, init_tk, init_zk, init_uk, rho, i
     end
 
     % ------ DATA INITIALIZATION ------ %
-    % Length of variables
-    n = size(q,1);
-    % Quantity of restrictions
-    n_c = size(h,1);
-    % Data initialization
-    if nargin > 4
-        tk = init_tk;
-        zk = init_zk;
-        uk = init_uk;
-    else
-        tk = zeros(n,1);
-        zk = zeros(n_c,1);
-        uk = zeros(n_c,1);
-    end
-    % ------ ADMM FORMULATION ------ %
+    tk = init_tk;
+    zk = init_zk;
+    uk = init_uk;
 
+    % ------ ADMM FORMULATION ------ %
     % Augmented Lagrangian scaled form
     % L_rho(x, z, u) = f(x) + g(z) + (rho/2)*||Ax + z - c + u||_2^2
     % where u -> y/rho (scaled dual variable)
@@ -73,17 +62,15 @@ function [tk, zk, uk] = fx_qp_admm(Q, q, H, h, init_tk, init_zk, init_uk, rho, i
         % In this case Z = [0, inf) so
         % Projection_Z(z) = max{z,0}
         
-    R = Q + rho*(H'*H);
+    R = Q + rho*(G'*G);
     R_inv = R \ eye(size(R,1));
     % Iterations
     for k = 1:iters
-        v_x = zk - h + uk;
-        tk = R_inv * (-rho*H'*v_x - q);    % update x
-           
-        zk = max(0, -H*tk - uk + h);             % update z
-
+        v_x = zk - g + uk;
+        tk = R_inv * (-rho*G'*v_x - q);    % update x
+        zk = max(0, -G*tk - uk + g);       % update z
         % Then we update the scaled dual variable
-        uk = uk + (H*tk + zk - h);                % update u
+        uk = uk + (G*tk + zk - g);         % update u
     end
 
 end
--- matlab/mpc_der_dense.m
+++ matlab/mpc_der_dense.m
@@ -1,7 +1,7 @@
 %% CONTROL MPC DE FILTRO LC CON RESTRICCIONES EN LA ENTRADA Y EN LOS ESTADOS
 % ===============================================================================
 % Alfonso Cortes Neira - Universidad Técnica Federico Santa María
-% 07-01-2024
+% 11-01-2024
 % Based on the work by Juan David Escárate
 % ===============================================================================
 
@@ -12,24 +12,23 @@ clc; clear;
 format('longE')
 
 N_HOR = 2;      % tamaño del horizonte de predicción
+IT_ADMM = 10;
 x0   = [0; 0; 0; 0]; % Estado inicial
 % rk: salida deseada del sistema
-rk = repelem([0 50; 0 0], [1,1], [1,2500]);
-% dk: perturbacion
-load('ADMM_model_10_iters.mat', 'io')
-dk = io';%*0.8;
+rk = repelem([0 100; 0 0], [1,1], [1,2500]);
+% RL Resistencia de carga
+RL = repelem([47.2 10 30 20], [500 1000 500 501]);
 
 % Parámetros del sistema
 Rf  = 0.065;    % Resistencia Filtro 
 Lf  = 3e-3;     % Inductancia Filtro 
 Cf  = 15e-6;    % Capacitancia Filtro 
-Vdc = 100;      % Voltaje DC de entrada, restricciones de voltaje en dq
+Vdc = 200;      % Voltaje DC de entrada, restricciones de voltaje en dq
 Ts  = 200e-6;   % Periodo de muestreo
 fb  = 50;       % Frecuencia Hz
-Wb  = 2*pi*fb;   % Frecuencia rad/s   
-RL  = 23.6;     % Resistencia de carga
+Wb  = 2*pi*fb;  % Frecuencia rad/s   
 Inom = 8;       % Corriente nominal, restricciones de corriente en dq
-tsimu = 0.5;       % tsimu: Tiempo de simulación en segundos
+tsimu = 0.5;        % tsimu: Tiempo de simulación en segundos
 k=0:Ts:tsimu-Ts;    % t: Arreglo de tiempo
 
 % Modelo espacio estado filtro LC (Continuo)
@@ -39,28 +38,32 @@ k=0:Ts:tsimu-Ts;    % t: Arreglo de tiempo
 %       x = [id; iq; vd; vq]
 %       d = [iod; ioq]
 %       u = -k*x_nau + u_s (Actuación MPC)
-Ac   = [-Rf/Lf      Wb  -1/Lf       0;
+A   = [-Rf/Lf      Wb  -1/Lf       0;
            -Wb  -Rf/Lf      0   -1/Lf;
           1/Cf       0      0      Wb;
              0     1/Cf   -Wb       0];
 
-Bc   = [  1/Lf      0;      
+B   = [  1/Lf      0;      
              0   1/Lf;
              0      0;
              0      0];
 
-Bdc  = [    0      0;      
+C = eye(4); % matriz de salidas
+
+Cu   = [0 0 1 0; 0 0 0 1]; % matriz de referencias
+
+Bp  = [    0      0;      
             0      0;
         -1/Cf      0;
             0  -1/Cf];
 
-C   = [0 0 1 0; 0 0 0 1];%eye(4);
+% dk = [0 0 1/RL 0; 0 0 0 1/RL]*xk
 
 % Modelo espacio estado filtro LC (Discreto)
 % x(k+1) = A*x(k) + B*u(k) + Bp*d(k) -> d = perturbacion (io, corriente de salida)
 % y(k)   = Cx(k)
-[~, Bd] = c2d(Ac,Bdc,Ts);
-[A, B] = c2d(Ac,Bc,Ts);
+[~, Bpd] = c2d(A,Bp,Ts);
+[Ad, Bd] = c2d(A,B,Ts);
 
 % Constraints
 % Restricciones de las entradas, voltajes [Vmd; Vmq] (politopo de 10 lados)
@@ -93,19 +96,17 @@ xmin = -xmax;
 % Matrices de Peso 
 Gamma      = 100*eye(2); %20*eye(2);    % 20 o 100 es por tuning
 Omega      = eye(4);
-Omega(1,1) = 1e2;       % Se le da mayor peso a los estados id iq
+Omega(1,1) = 1e2;       % Se le da mayor peso a los estados id iq (corrientes)
 Omega(2,2) = 1e2;
 
 % Solución a LQR discreto
-[~,OmegaN,~] = dlqr(A,B,Omega,Gamma);
-
-IT_ADMM = 10;
+[~,OmegaN,~] = dlqr(Ad,Bd,Omega,Gamma);
 
 %% Dense Formulation
 
-N_SYS = size(A,1);      % numero de estados
-M_SYS = size(B,2);      % numero de actuaciones
-P_SYS = size(C,1);      % numero de salidas
+N_SYS = size(Ad,1);      % numero de estados
+M_SYS = size(Bd,2);      % numero de actuaciones
+P_SYS = size(C,1);      % numero de salidas con referencia
 A_SYS = size(umax,1);   % numero de pares de restricciones de la entrada
 B_SYS = size(xmax,1);   % numero de pares de restricciones del estado
 C_SYS = 2*(A_SYS+B_SYS);% numero total de restricciones 
@@ -114,11 +115,13 @@ N_QP = N_HOR * M_SYS;
 M_QP = N_HOR * C_SYS;
 xk = zeros(N_SYS, length(k), 'single');
 uk = zeros(M_SYS, length(k), 'single');
+dk = zeros(M_SYS, length(k), 'single');
 xk(:,1) = x0;
-
+dk(:,1) = [0 0 1/RL(1) 0; 0 0 0 1/RL(1)]*x0;
+J = zeros(length(k));
 %theta = zeros(N_QP, length(k), 'single');
 
-[D,E] = fx_dense_matrices(A,B,N_HOR);       % constantes del sistema
+[D,E] = fx_dense_matrices(Ad,Bd,N_HOR);       % constantes del sistema
 K = blkdiag(kron(eye(N_HOR-1),Omega),OmegaN);
 L = kron(eye(N_HOR),Gamma); 
 Q = 2*(L+E'*K*E);                           % constante del sistema
@@ -130,7 +133,7 @@ F = single(2*D'*K*E);                       % constante del sistema
 D = single(D);
 G = [V;-V;W*E;-W*E];                        % constante del sistema
 
-rho = single(62.963413);%single(fx_dhang_rho(Q,G));%single(0.10070947);%
+rho = single(fx_dhang_rho(Q,G));%single(62.963413);%single(0.10070947);%
 
 Q = single(Q);
 G = single(G);
@@ -143,25 +146,28 @@ z_ADMM = zeros(M_QP, 1, 'single');
 u_ADMM = zeros(M_QP, 1, 'single');
 
 for i=1:length(k)
-    [xinf, uinf] = fx_stationary(A, B, C, rk(:,i), Bd, dk(:,i));
+    dk(:,i) = [0 0 1/RL(i) 0; 0 0 0 1/RL(i)]*xk(:,i);
+    [xinf, uinf] = fx_stationary(Ad, Bd, Cu, rk(:,i), Bpd, dk(:,i));
     q = ((xk(:,i)-xinf)'*F)';
-    Vu = single(V*kron(ones(N_HOR,1),uinf));
-    WDx = single(W*(D*xk(:,i) + kron(ones(N_HOR,1),xinf)));
-    c = kron(ones(N_HOR,1),single(umax));       % constante del sistema
-    d = kron(ones(N_HOR,1),single(umin));       % constante del sistema
-    e = kron(ones(N_HOR,1),single(xmax));       % constante del sistema
-    f = kron(ones(N_HOR,1),single(xmin));       % constante del sistema
-    g = [c-Vu; Vu-d; e-WDx; WDx-f];
+    Huinf = H*uinf;
+    Ixinf = I*xinf;
+    WDx = single(W*D*(xk(:,i)-xinf));
+    c = kron(ones(N_HOR,1),single(umax-Huinf));       % 
+    d = kron(ones(N_HOR,1),single(Huinf-umin));       % 
+    e = kron(ones(N_HOR,1),single(xmax-Ixinf));       % 
+    f = kron(ones(N_HOR,1),single(Ixinf-xmin));       % 
+    g = [c; d; e-WDx; f+WDx];
     [t_ADMM, z_ADMM, u_ADMM] = fx_qp_admm(Q, q, G, g,t_ADMM, z_ADMM, u_ADMM, rho, IT_ADMM);
-    uk(:,i) = t_ADMM(1:M_SYS)+uinf;
-    xk(:,i+1) = A*xk(:,i)+B*uk(:,i)+Bd*dk(:,i); % Cálculo del siguiente estado
+    uk(:,i) = t_ADMM(1:M_SYS) + uinf;
+    xk(:,i+1) = Ad*xk(:,i)+Bd*uk(:,i)+Bpd*dk(:,i); % Cálculo del siguiente estado
     %theta(:,i) = t_ADMM(:,1);
+    J(i) = t_ADMM'*Q*t_ADMM/2 + q'*t_ADMM;
 end
 
 R = Q + rho*(G'*G);
 R_inv = R \ eye(size(R,1));
-P = -rho*G';            % RhoHt_neg
-T = [A-eye(N_SYS),B;C,zeros(M_SYS,M_SYS)];
+P = -rho*G';            % RhoGt_neg
+T = [Ad-eye(N_SYS),Bd;Cu,zeros(M_SYS,M_SYS)];
 T_inv = T \ eye(N_SYS+M_SYS);
 
 %% Plot
@@ -178,6 +184,23 @@ plot(uk(2,:))
 grid on
 legend('Vrefd', 'Vrefq', '|Ifdq| (x0 and x1)', 'Vcd (x2)', 'Vcq (x3)','Input u0','Input u1')
 
+%% Plot 2
+
+figure
+plot(uk(2,:), uk(1,:))
+hold on
+plot(Vdc/(sqrt(3))*sin(0:pi/100:2*pi),Vdc/(sqrt(3))*cos(0:pi/100:2*pi))
+grid on
+hold off
+%legend('|Vdq|', 'Inom')
+
+figure
+plot(xk(2,:), xk(1,:))
+hold on
+plot(8*sin(0:pi/100:2*pi),8*cos(0:pi/100:2*pi))
+grid on
+hold off
+legend('I', 'Inom')
 
 %% Generate C++ file with Global Variables (constants)
 
@@ -187,24 +210,26 @@ txtfileID = fopen(txtfile,'w');
 fprintf(txtfileID, "\n#include "+char(34)+"system.hpp"+char(34)+"\n\n// HOR = "+N_HOR+"\n#if defined DENSE\n\n");
 
 fx_cpp_print_matrix(txtfileID, G, "data_t G[M_QP][N_QP]", M_QP, N_QP)
-fx_cpp_print_matrix(txtfileID, c, "data_t c[A_SYS*N_HOR]", A_SYS*N_HOR)
-fx_cpp_print_matrix(txtfileID, d, "data_t d[A_SYS*N_HOR]", A_SYS*N_HOR)
-fx_cpp_print_matrix(txtfileID, e, "data_t f[B_SYS*N_HOR]", (B_SYS*N_HOR))
-fx_cpp_print_matrix(txtfileID, f, "data_t e[B_SYS*N_HOR]", (B_SYS*N_HOR))
-fx_cpp_print_matrix(txtfileID, D, "data_t D[N_SYS*N_HOR][N_SYS]", (N_SYS*N_HOR), N_SYS)
+%fx_cpp_print_matrix(txtfileID, c, "data_t c[A_SYS*N_HOR]", A_SYS*N_HOR)
+%fx_cpp_print_matrix(txtfileID, d, "data_t d[A_SYS*N_HOR]", A_SYS*N_HOR)
+%fx_cpp_print_matrix(txtfileID, e, "data_t f[B_SYS*N_HOR]", (B_SYS*N_HOR))
+%fx_cpp_print_matrix(txtfileID, f, "data_t e[B_SYS*N_HOR]", (B_SYS*N_HOR))
+%fx_cpp_print_matrix(txtfileID, D, "data_t D[N_SYS*N_HOR][N_SYS]", (N_SYS*N_HOR), N_SYS)
 fx_cpp_print_matrix(txtfileID, F, "data_t F[N_SYS][N_QP]", N_SYS, N_QP)
 
 fx_cpp_print_matrix(txtfileID, umin, "data_t umin[A_SYS]", A_SYS)
 fx_cpp_print_matrix(txtfileID, umax, "data_t umax[A_SYS]", A_SYS)
 fx_cpp_print_matrix(txtfileID, xmin, "data_t xmin[B_SYS]", B_SYS)
 fx_cpp_print_matrix(txtfileID, xmax, "data_t xmax[B_SYS]", B_SYS)
-fx_cpp_print_matrix(txtfileID, V, "data_t V[A_SYS*N_HOR][N_QP]", A_SYS*N_HOR, N_QP)
-fx_cpp_print_matrix(txtfileID, W, "data_t W[B_SYS*N_HOR][N_QP]", B_SYS*N_HOR, N_QP)
+fx_cpp_print_matrix(txtfileID, H, "data_t H[A_SYS][M_SYS]", A_SYS, M_SYS)
+fx_cpp_print_matrix(txtfileID, I, "data_t I[B_SYS][N_SYS]", B_SYS, N_SYS)
+%fx_cpp_print_matrix(txtfileID, V, "data_t V[A_SYS*N_HOR][N_QP]", A_SYS*N_HOR, N_QP)
+%fx_cpp_print_matrix(txtfileID, W, "data_t W[B_SYS*N_HOR][N_SYS*N_HOR]", B_SYS*N_HOR, N_QP)
+fx_cpp_print_matrix(txtfileID, W*D, "data_t WD[B_SYS*N_HOR][N_SYS]", B_SYS*N_HOR, N_QP)
 
 fx_cpp_print_matrix(txtfileID, R_inv, "data_t R_inv[N_QP][N_QP]", N_QP, N_QP)
 fx_cpp_print_matrix(txtfileID, T_inv, "data_t T_inv[N_SYS+M_SYS][N_SYS+M_SYS]", N_SYS+M_SYS, N_SYS+M_SYS)
-fx_cpp_print_matrix(txtfileID, P, "data_t W[N_QP][M_QP]", N_QP, M_QP)
-
+fx_cpp_print_matrix(txtfileID, P, "data_t P[N_QP][M_QP]", N_QP, M_QP)
 
 fprintf(txtfileID, "\n#else\n\n// SPARSE\n\n#endif");
 
@@ -218,8 +243,8 @@ binfileID = fopen(binfile,'w');
 
 nSamples = length(k);
 data_t = 'single';
-A = single(A);
-B = single(B);
+Ad = single(Ad);
+Bd = single(Bd);
 
 fwrite(binfileID, N_SYS,'uint8');
 fwrite(binfileID, M_SYS,'uint8');
@@ -236,8 +261,8 @@ fwrite(binfileID, nSamples,'uint16');
 % fwrite(binfileID,reshape(umax',1,[]),data_t);
 % fwrite(binfileID,reshape(Q',1,[]),data_t);
 % fwrite(binfileID,reshape(H',1,[]),data_t);
-fwrite(binfileID,reshape(A',1,[]),data_t);
-fwrite(binfileID,reshape(B',1,[]),data_t);
+fwrite(binfileID,reshape(Ad',1,[]),data_t);
+fwrite(binfileID,reshape(Bd',1,[]),data_t);
 % fwrite(binfileID,rho,data_t);
 % fwrite(binfileID,reshape(R_inv',1,[]),data_t);
 % fwrite(binfileID,reshape(RhoHt_neg',1,[]),data_t);
